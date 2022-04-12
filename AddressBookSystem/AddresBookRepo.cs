f7760c28bebaac9e78bde1fd3bb4300d24a7b633
payalbanode0 <payalbanode0@gmail.com>
2022-04-12 13:58:36 +0530

    get range

--- AddressBookSystem/AddresBookRepo.cs
+++ AddressBookSystem/AddresBookRepo.cs
@@ -71,8 +71,84 @@ namespace AddressBookSystem
                 this.connection.Close();
             }
         }
+        //UC 17
+        public void Update(AddressBookModel addressBookModel)
+        {
+            string query = @"Update Address_Book_Table Set RelationType ='Me' Where FirstName='Manoj'";
+            SqlCommand cmd = new SqlCommand(query, this.connection);
+            this.connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            AddressBookModel addressBook = new AddressBookModel();
+            Console.WriteLine("Update Successfull");
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    addressBook.FirstName = dr.GetString(0);
+                    addressBook.RelationType = dr.GetString(8);
+                    //display 
+
+                    Console.WriteLine("FirstName : " + "{0}" + "Relation Type : " + "{8}", addressBookModel.FirstName, addressBookModel.RelationType);
+
+
+                }
+            }
+            dr.Close();
+            this.connection.Close();
+        }
+        //UC 18 - Alter 
+        public void Alter(AddressBookModel model)
+        {
+            string query = @"Alter Table Address_Book_Table Add StartDate datetime default GetDate() Not Null";
+            SqlCommand cmd = new SqlCommand(query, this.connection);
+            this.connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            Console.WriteLine("Alter Successfull Added StartDate Column");
+            dr.Close();
+            this.connection.Close();
+
+        }
+        //UC 18 Get Date
+        public void GetDateRange(AddressBookModel model)
+        {
+            string query = @"Select * From Address_Book_Table Where StartDate Between '01-01-2000' And GetDate()";
+            SqlCommand cmd = new SqlCommand(query, this.connection);
+            this.connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            AddressBookModel addressBookModel = new AddressBookModel();
+            Console.WriteLine("Records in Data Range\n");
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    addressBookModel.FirstName = dr.GetString(0);
+                    addressBookModel.LastName = dr.GetString(1);
+                    addressBookModel._address = dr.GetString(2);
+                    addressBookModel.City = dr.GetString(3);
+                    addressBookModel._State = dr.GetString(4);
+                    addressBookModel.Zip = dr.GetInt32(5);
+                    addressBookModel.PhoneNumber = dr.GetString(6);
+                    addressBookModel.email = dr.GetString(7);
+                    addressBookModel.RelationType = dr.GetString(8);
+                    addressBookModel.StartDate = dr.GetDateTime(9);
+
+                    Console.WriteLine("FirstName : " + "{0}" + ", Last Name : " + "{1}" + ", Address : " + "{2}" + ", City : " + "{3}" + ", State" + "{4}" + ", Zip : " + "{5}" + ", PhoneNumber : " + "{6}" + ", Email : " + "{7}" + ", Relation Type : " + "{8}" + ", Start Date : " + "{9}", addressBookModel.FirstName, addressBookModel.LastName, addressBookModel._address, addressBookModel.City, addressBookModel._State, addressBookModel.Zip, addressBookModel.PhoneNumber, addressBookModel.email, addressBookModel.RelationType, addressBookModel.StartDate);
+
+                }
+            }
+            dr.Close();
+            this.connection.Close();
+        }
 
     }
 
-
 }
+
+
+   
+
+    
+
+
+
